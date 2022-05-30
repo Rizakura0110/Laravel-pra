daee8acd184a158689e940120644323a75583fc5
ryo sasakura <rasakura0110@gmail.com>
2022-05-31 08:52:08 +0900

    マイグレーションファイル追加

---
+++ database/migrations/2022_05_30_234430_create_train_names_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('train_names', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(false)->comment('便名');
+            $table->time('start_at')->nullable(false)->comment('出発時刻');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('train_names');
+    }
+};
